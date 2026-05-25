4c60e2895e551e929183dc35addd4dba91e503bb
vi3lman <cesarvielman123@gmail.com>
2026-05-24 21:22:06 -0300

    Intento fallido de ordenar get/post para que quede bonito, pero igual funca no se preocupen

--- app/main.py
+++ app/main.py
@@ -36,20 +36,46 @@ def read_root():
     return {"message": "Bienvenido a la API de Estacionamiento"}
 
 
-# Endpoints para Calles y Espacios
-
 @app.get("/calles", response_model=list[CalleRead])
 def read_calles(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     calles = get_calles(db, skip=skip, limit=limit)
     return calles
+
+
+@app.get("/conductores", response_model=list[ConductorRead])
+def get_conductores_endpoint(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return get_conductores(db, skip=skip, limit=limit)
 
 @app.get("/espacios/{id_calle}", response_model=list[EspacioRead])
 def read_espacios_por_calle(id_calle: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     espacios = get_espacios_por_calle(db, id_calle=id_calle, skip=skip, limit=limit)
     return espacios
 
+@app.get("/espacios/libres/{id_calle}", response_model=list[EspacioRead])
+def get_espacios_libres_por_calle_endpoint(id_calle: int, fecha_hora_ingreso: datetime, tiempo_estimado: int, db: Session = Depends(get_db)):
+    if fecha_hora_ingreso < datetime.now():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="La fecha y hora de ingreso no puede ser en el pasado."
+        )
+    espacios_libres = get_espacios_libres_por_calle(db=db, id_calle=id_calle, fecha_hora_ingreso=fecha_hora_ingreso, tiempo_estimado=tiempo_estimado)
+    return espacios_libres
 
-@app.post("/calles/", response_model=CalleRead)
+@app.get("/ocupaciones", response_model=list[OcupacionRead])
+def get_ocupaciones_endpoint(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return get_ocupaciones(db, skip=skip, limit=limit)
+
+@app.get("/ocupaciones/{id_conductor}", response_model=list[Dict])
+def get_ocupaciones_por_conductor_endpoint(id_conductor: int, db: Session = Depends(get_db)):
+    ocupaciones = get_ocupaciones_por_conductor(db, id_conductor=id_conductor)
+    if not ocupaciones:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"No se encontraron ocupaciones para el conductor con ID {id_conductor}."
+        )
+    return ocupaciones
+
+@app.post("/calles", response_model=CalleRead)
 def create_calle_endpoint(calle: CalleCreate, db: Session = Depends(get_db)):
     return create_calle(db=db, calle=calle)
 
@@ -81,38 +107,13 @@ def crear_ocupacion(ocupacion: OcupacionCreate,db: Session = Depends(get_db)):
             status_code=409,
             detail="El espacio ya está ocupado en ese horario o la fecha ingresada no es válida."
         )
-
     return create_ocupacion(db, ocupacion)
 
-@app.get("/espacios/libres/{id_calle}", response_model=list[EspacioRead])
-def get_espacios_libres_por_calle_endpoint(id_calle: int, fecha_hora_ingreso: datetime, tiempo_estimado: int, db: Session = Depends(get_db)):
-    if fecha_hora_ingreso < datetime.now():
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, 
-            detail="La fecha y hora de ingreso no puede ser en el pasado."
-        )
-    espacios_libres = get_espacios_libres_por_calle(db=db, id_calle=id_calle, fecha_hora_ingreso=fecha_hora_ingreso, tiempo_estimado=tiempo_estimado)
-    return espacios_libres
 
-
-#Todo lo relacionado con ocupaciones
 @app.post("/ocupaciones")
 def crear_ocupacion(ocupacion: OcupacionCreate,db: Session = Depends(get_db)):
     return create_ocupacion(db, ocupacion)
 
-@app.get("/ocupaciones", response_model=list[OcupacionRead])
-def get_ocupaciones_endpoint(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return get_ocupaciones(db, skip=skip, limit=limit)
-
-@app.get("/ocupaciones/{id_conductor}", response_model=list[Dict])
-def get_ocupaciones_por_conductor_endpoint(id_conductor: int, db: Session = Depends(get_db)):
-    ocupaciones = get_ocupaciones_por_conductor(db, id_conductor=id_conductor)
-    if not ocupaciones:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"No se encontraron ocupaciones para el conductor con ID {id_conductor}."
-        )
-    return ocupaciones
 
 
 @app.post("/espacios/{id_espacio}/infraccion", response_model=OcupacionRead)
@@ -139,7 +140,3 @@ def marcar_salida_endpoint(id_ocupacion: int, db: Session = Depends(get_db)):
         )
     return ocupacion_finalizada
 
-
-@app.get("/conductores", response_model=list[ConductorRead])
-def get_conductores_endpoint(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    return get_conductores(db, skip=skip, limit=limit)
